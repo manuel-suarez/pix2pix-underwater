8b03ef8a2e20645d05222f7001c951d1edbcb46f
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-05 11:01:37 -0500

    Losses

--- main.py
+++ main.py
@@ -255,3 +255,23 @@ def Discriminator():
     return tf.keras.Model(inputs=[inp, tar], outputs=last)
 discriminator = Discriminator()
 
+loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+def discriminator_loss(disc_real_output, disc_generated_output):
+    real_loss        = loss_object(tf.ones_like(disc_real_output), disc_real_output)
+    generated_loss   = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
+    total_disc_loss  = real_loss + generated_loss
+    return total_disc_loss
+LAMBDA = 100
+def generator_loss(disc_generated_output, gen_output, target):
+    '''
+    el generador debe entrenarse para maximizar los errores de detección de imágenes sintéticas
+    '''
+    # Entropia cruzada a partir de logits
+    gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
+
+    # Media de los Errores Absolutos
+    l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+
+    total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+
+    return total_gen_loss, gan_loss, l1_loss
