ccd918c68231849ba8e05f395dadc17b351cf391
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-05 10:19:54 -0500

    Test of save figure

--- main.py
+++ main.py
@@ -27,3 +27,73 @@ xfiles=np.array(xfiles)
 yfiles=np.array(yfiles)
 
 [print(x, y) for x,y in zip(xfiles[:5],yfiles[:5])];
+
+BUFFER_SIZE      = len(xfiles)
+steps_per_epoch  = BUFFER_SIZE //BATCH_SIZE
+print('num image files : ', BUFFER_SIZE)
+print('steps per epoch : ', steps_per_epoch)
+
+
+def load_images(xfile, yfile, flip=True):
+    '''
+    Lee par de imagenes jpeg y las reescala la tamaño deseado
+
+    Aumantación: Flip horizontal aleatorio, sincronizado
+    '''
+
+    xim = tf.io.read_file(xfile)
+    xim = tf.image.decode_bmp(xim)
+    xim = tf.cast(xim, tf.float32)
+    xim = xim / 127.5 - 1
+    # en caso de ser necesario cambiar las dimensiones de la imagen x al leerla
+    xim = tf.image.resize(xim, INPUT_DIM[:2],
+                          method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+    yim = tf.io.read_file(yfile)
+    yim = tf.image.decode_bmp(yim)
+    yim = tf.cast(yim, tf.float32)
+    yim = yim / 127.5 - 1
+    # en caso de ser necesario cambiar las dimensiones de la imagen y al leerla
+    yim = tf.image.resize(yim, INPUT_DIM[:2],
+                          method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+    # Aumentación sincronizada de dada imágen del par $(x,y)$, en este caso solo un flip der-izq
+    if flip and tf.random.uniform(()) > 0.5:
+        xim = tf.image.flip_left_right(xim)
+        yim = tf.image.flip_left_right(yim)
+
+    return xim, yim
+
+
+def display_images(fname, x_imgs=None, y_imgs=None, rows=4, cols=3, offset=0):
+    '''
+    Despliega pares de imágenes tomando una de cada lista
+    '''
+    plt.figure(figsize=(cols * 5, rows * 2.5))
+    for i in range(rows * cols):
+        plt.subplot(rows, cols * 2, 2 * i + 1)
+        plt.imshow((x_imgs[i + offset] + 1) / 2)
+        plt.axis('off')
+
+        plt.subplot(rows, cols * 2, 2 * i + 2)
+        plt.imshow((y_imgs[i + offset] + 1) / 2)
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(fname)
+
+
+rows = 2
+cols = 2
+
+x_imgs = []
+y_imgs = []
+
+for i in range(rows * cols):
+    xim, yim = load_images(xfiles[i], yfiles[i])
+    x_imgs.append(xim)
+    y_imgs.append(yim)
+
+print(x_imgs[0].shape, x_imgs[0].shape)  # a modo de comprobacion
+
+display_images('testfigure1.png', x_imgs, y_imgs, rows=rows, cols=cols)
